2381bbb4cda93b589adc99fbee1b911ff2116001
Matt Taube <mctaube@users.noreply.github.com>
2017-05-29 10:34:49 -0500

    Through step 13

---
+++ app/models/comment.rb
@@ -0,0 +1,9 @@
+class Comment < ApplicationRecord
+
+  validates :user_id, :presence => true
+  validates :photo_id, :presence => true
+  validates :body, :presence => true
+
+belongs_to :photo
+
+end
