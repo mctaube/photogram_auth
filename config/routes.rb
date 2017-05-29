d8253a1f233780d4bf0c448a17133e2bc3ec5895
Matt Taube <mctaube@users.noreply.github.com>
2017-05-29 14:31:34 -0500

    middle of step 17

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # Routes for the user resource:
+  # READ
+  get "/users", :controller => "users", :action => "index"
+  get "/users/:id", :controller => "users", :action => "show"
+
+
   # Routes for the Comment resource:
   root "photos#index"
 
